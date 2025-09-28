fe0ed7aed7bd0ca97a9f5b3e60ea2fccbc944ddf
Saurabh_Jangid <sourabhjangid584@gmail.com>
2025-09-28 23:06:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-```javascript
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-"./pages/**/*.{js,jsx}",
-"./components/**/*.{js,jsx}"
-],
-theme: { extend: {} },
-plugins: []
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
-```
