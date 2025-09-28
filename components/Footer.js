b431dec0c96378ac08a5af44f5095021eb351c06
Saurabh_Jangid <sourabhjangid584@gmail.com>
2025-09-28 22:57:42 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center bg-gray-100">
+      <p className="text-gray-600">&copy; 2025 AI SaaS Landing. All rights reserved.</p>
+    </footer>
+  )
+}
