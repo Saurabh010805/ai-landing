f65c190503c42553d58bd82ae528a3b1e6c9fa87
Saurabh_Jangid <sourabhjangid584@gmail.com>
2025-09-28 22:43:36 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero'
+import Pricing from '../components/Pricing'
+import FAQ from '../components/FAQ'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </div>
+  )
+}
